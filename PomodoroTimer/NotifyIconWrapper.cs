67e702908f06a04408c57e2b2876b3bdc4f3d5af
Norbert Janas <norbertjanas99@gmail.com>
2021-08-01 23:49:10 +0200

    Add reference to windows forms controls

---
+++ PomodoroTimer/NotifyIconWrapper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using System.ComponentModel;
+
+namespace PomodoroTimer
+{
+    public class NotifyIconWrapper : FrameworkElement, IDisposable
+    {
+        private readonly NotifyIcon _notifyIcon;
+
+        public NotifyIconWrapper()
+        {
+            if (DesignerProperties.GetIsInDesignMode(this))
+                return;
+            _notifyIcon = new NotifyIcon
+            {
+
+            };
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
